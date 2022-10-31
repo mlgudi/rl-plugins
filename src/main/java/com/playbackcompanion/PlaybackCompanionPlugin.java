d161bfbc64a0e82004aafd479952ff78df4d6179
mlgudi <seriousgudi@gmail.com>
2022-10-31 11:46:03 +0000

    Initial commit

--- src/main/java/com/playbackcompanion/PlaybackCompanionPlugin.java
+++ src/main/java/com/playbackcompanion/PlaybackCompanionPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.playbackcompanion;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Playback Companion"
 )
-public class ExamplePlugin extends Plugin
+public class PlaybackCompanionPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private PlaybackCompanionConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	PlaybackCompanionConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(PlaybackCompanionConfig.class);
 	}
 }
